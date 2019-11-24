740794e323792705c29a6402aeee00f3c6782b75
EuphyD <47607574+EuphyD@users.noreply.github.com>
2019-11-24 02:26:01 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,33 +1,92 @@
 import React from 'react';
-//import logo from './logo.svg';
 import './App.css';
-//
 import { Component} from 'react';
-import Toolbar from './components/Toolbar/Toolbar';
-import sideDrawer from './components/SideDrawer/SideDrawer';
+//import UnicornHeart from "../src/site_media/Bottom Menu_Home_Unicorn Heart.png"
+//import Toolbar from './components/Toolbar/Toolbar';
+//import sideDrawer from './components/SideDrawer/SideDrawer';
 
 class App extends Component{
   render(){
     return(
-      <div style = {{height: '100%'}}/*className="App"*/>
-        <Toolbar />
-        <sideDrawer/>
-        <main style= {{marginTop:'64px'}}>
-          <div className="content">
-          <h1 className="heading">Heading 1</h1>
-          <h2 className="subheading">
-            Subheading</h2>
+    <div className="App">
+      <div class="content-wrapper">
+        <header class="header">
+          <div class="header__logo main">
+            <a href="/">Unicorn Chairty Club</a>
           </div>
-          <p>Page content!</p>
-        </main>
+          <div class="header__toolbar logged-in">Toolbar here.</div>
+        </header>
+        <nav class="header__nav children-base" role="navigation">
+          <div class="navi_list">
+            <li class="nav__item  nav__item--active" >
+              <div class="nav__icon nav__icon--library"></div>
+              <div class="nav__label">Project</div>
+            </li>
+            <li class="nav__item " >
+              <div class="nav__icon nav__icon--library"></div>
+              <div class="nav__label">Spotlight</div>
+            </li>
+            <li class="nav__item ">
+              <div class="nav__icon nav__icon--library"></div>
+              <div class="nav__label">HomeIcon</div>
+            </li>
+            <li class="nav__item ">
+              <div class="nav__icon nav__icon--library"></div>
+              <div class="nav__label">Friends</div>
+            </li>
+            <li class="nav__item ">
+              <div class="nav__icon nav__icon--library"></div>
+              <div class="nav__label">Yearbook</div>
+            </li>
+          </div>
+        </nav>
+        <div class="container">
+          
+          <div class="Tool-guide">
+            <div class="child__avatar">Tool Guide here.</div>
+          </div>
+          <div class="content">
+            <div class="schedule">
+              <div class="page-title">
+                <h1>This is Box</h1></div>
+              <div class="page-undertitle">This is post.</div>
+            </div>
+          </div>
+          <div class="sidebar-profile">
+            <div class="profile__wrapper">
+              <div class="profile__info">
+                <a href="/children/update/352/">
+                  <div class="child__avatar">Picture here.</div>
+                </a>
+                <div class="profile__name">
+                  <a href="/children/update/352/">Name</a>
+                </div>
+                <div class="profile_Unicorn_Name">Unicorn Name</div>
+                <div class="profile_blessingGroup">Blessing Group</div>
+                <div class="profile_Color_rank">Color Horn Rank</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+    </div>
     );
   }
 }
 
 /*
 function App() {
-  return (
+      <div style = {{height: '100%'}}/*className="App"
+      <Toolbar />
+      <sideDrawer/>
+      <main style= {{marginTop:'64px'}}>
+        <div className="content">
+        <h1 className="heading">Heading 1</h1>
+        <h2 className="subheading">
+          Subheading</h2>
+        </div>
+        <p>Page content!</p>
+      </main>
     <div className="App">
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -44,28 +103,6 @@ function App() {
           <h3>Heading 3</h3>
        </div>
      </div>
-    
-
-
-
-
-
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
     */
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-//add fonts
 
 //ReactDOM.render(What do I want to render, where do I want to render it#ID);
 ReactDOM.render(<App />, document.getElementById('root'));
