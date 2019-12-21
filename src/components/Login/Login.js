2fd8fea73d63d213610a5bcaa120239106303556
santoshmedimi <57875878+santoshmedimi@users.noreply.github.com>
2019-12-21 07:43:52 -0500

    Add files via upload
    added auth to login.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import "./Login.css";
-import { NavLink } from "react-router-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import Homepage from "../Homepage/Homepage";
 import Logo from "../../site_media/Logo_Horizontal_No_Tagline.png";
 import Facebook_img from "../../site_media/facebook.png";
-
+import Register from "../Register/Register";
+import firebaseapi from "../../config/firebaseapi";
 class Login extends React.Component {
   // constructor(props) {
   //   super(props);
@@ -108,7 +109,24 @@ export default Login;
 class LoginForm extends React.Component {
   constructor(props) {
     super(props);
+    this.login = this.login.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
     this.state = { email: "", password: "", errors: [] };
+  }
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  login(e) {
+    e.preventDefault();
+    firebaseapi
+      .auth()
+      .signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then(u => {})
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   showValidationErr(elm, msg) {
@@ -160,67 +178,68 @@ class LoginForm extends React.Component {
     }
 
     return (
-      <Router>
-        <div className="login-form">
-          <form
-            className="login__form"
-            name="form"
-            method="POST"
-            onSubmit={this.handleSubmit}
-          >
-            <input
-              type="hidden"
-              name="csrfmiddlewaretoken"
-              value="Aegb7wMJ3xjt52OHuAdxdkKr6P5S5BFt"
-            />
+      <div className="login-form">
+        <form className="login__form" name="form" method="POST">
+          <input
+            type="hidden"
+            name="csrfmiddlewaretoken"
+            value="Aegb7wMJ3xjt52OHuAdxdkKr6P5S5BFt"
+          />
 
-            <div class="form-item">
-              <input
-                // maxlength="254"
-                name="username"
-                type="text"
-                id="email"
-                placeholder="Email"
-                // value={this.state.value}
-                onChange={this.onEmailChange.bind(this)}
-              />
-              <small className="danger-error">{emailErr ? emailErr : ""}</small>
-              <ul class="errorlist">
-                <li></li>
-              </ul>
-            </div>
-            <div class="form-item">
-              <input
-                name="password"
-                type="password"
-                id="password"
-                placeholder="Password"
-                onChange={this.onPasswordChange.bind(this)}
-              />
-              <small className="danger-error">
-                {passwordErr ? passwordErr : ""}
-              </small>
-              <ul class="errorlist">
-                <li></li>
-              </ul>
-            </div>
-            <NavLink to={"/Homepage"}>
-              <div class="login__field">
-                <button
-                  type="button"
-                  className="submit-button"
-                  onClick={this.submitLogin.bind(this)}
-                >
-                  Log In
-                </button>
-              </div>
+          <div class="form-item">
+            <input
+              name="username"
+              id="email"
+              placeholder="Email"
+              input
+              value={this.state.email}
+              onChange={this.handleChange}
+              type="email"
+              name="email"
+            />
+            <small className="danger-error">{emailErr ? emailErr : ""}</small>
+            <ul class="errorlist">
+              <li></li>
+            </ul>
+          </div>
+          <div class="form-item">
+            <input
+              name="password"
+              type="password"
+              id="password"
+              placeholder="Password"
+              input
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+            <small className="danger-error">
+              {passwordErr ? passwordErr : ""}
+            </small>
+            <ul class="errorlist">
+              <li></li>
+            </ul>
+          </div>
+
+          <div class="login__field">
+            <button
+              type="button"
+              onClick={this.login}
+              className="submit-button"
+            >
+              Log In
+            </button>
+            <Router>
+              <Route path="/Register" component={Register} />
+            </Router>
+
+            <NavLink to="/Register">
+              <button type="button" className="submit-button">
+                Don't have an account?{" "}
+              </button>
             </NavLink>
-          </form>
-          <Route path="/Homepage" exact component={Homepage}>
-            <Homepage />
-          </Route>
-        </div>
-      </Router>
+          </div>
+        </form>
+      </div>
     );
   }
 }
