bc65725666291f80c08f9834b7d67dff2579621a
santoshmedimi <57875878+santoshmedimi@users.noreply.github.com>
2019-12-21 07:42:38 -0500

    Add files via upload
    added login

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import Mobile_toolbar from "./components/Mobile_toolbar/Mobile_toolbar";
 import Login from "./components/Login/Login";
 import ChildForm from "./components/MyChildren/ChildForm";
 import Register from "./components/Register/Register";
-
+import firebaseapi from "./config/firebaseapi";
 //import UnicornHeart from "../src/site_media/Bottom Menu_Home_Unicorn Heart.png"
 //import Toolbar from './components/Toolbar/Toolbar';
 //import sideDrawer from './components/SideDrawer/SideDrawer';
@@ -27,36 +27,52 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showSideBar: true
+      user: {}
     };
   }
-  setToFalse(props) {
-    this.state.showSideBar = false;
+  componentDidMount() {
+    this.authlistener();
+  }
+  authlistener() {
+    firebaseapi.auth().onAuthStateChanged(user => {
+      // console.log(user);
+      if (user) {
+        this.setState({ user });
+        //  localStorage.setItem('user', user.uid);
+      } else {
+        this.setState({ user: null });
+        //   localStorage.removeItem('user');
+      }
+    });
   }
 
   render() {
     return (
       <Router>
-        <div className="App">
-          <div className="content-wrapper">
-            {/* <Register /> */}
-            {/* <Login /> */}
-            <Header />
-            <Navbar />
+        {this.state.user ? (
+          <div className="App">
+            <div className="content-wrapper">
+              <Header />
+              <Navbar />
 
-            <div className="container">
-              <Switch>
-                <Route path="/" exact component={Homepage} />
-                <Route path="/Projects" component={Projects} />
-                <Route path="/Spotlight" component={Spotlight} />
-                <Route path="/Friends" component={Friends} />
-                <Route path="/Yearbook" component={Yearbook} />
-              </Switch>
-              {this.state.showSideBar ? <Sidebar /> : null}
+              <div className="container">
+                <Switch>
+                  <Route path="/" exact component={Homepage} />
+                  <Route path="/Projects" component={Projects} />
+                  <Route path="/Spotlight" component={Spotlight} />
+                  <Route path="/Friends" component={Friends} />
+                  <Route path="/Yearbook" component={Yearbook} />
+                </Switch>
+                <Sidebar />
+              </div>
             </div>
+            <Footer />{" "}
           </div>
-          <Footer />
-        </div>
+        ) : (
+          <div className="App">
+            <Login />{" "}
+          </div>
+        )}
       </Router>
     );
   }
