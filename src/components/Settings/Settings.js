b36828990744558ead9372786a30cbebca0426a3
santoshmedimi <57875878+santoshmedimi@users.noreply.github.com>
2019-12-21 07:44:59 -0500

    Add files via upload
    added logout

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
 import Setting_icon from "../../image/settings.png";
 import "./Settings.css";
-
+import firebaseapi from "../../config/firebaseapi";
 class Settings extends Component {
+  constructor(props) {
+    super(props);
+    this.logout = this.logout.bind(this);
+  }
+  logout() {
+    firebaseapi.auth().signOut();
+  }
   render() {
     return (
       <div className="header__settings">
@@ -13,7 +20,7 @@ class Settings extends Component {
             <a href="/" className="logout-link">
               <div className="settings-dropdown__item">My&nbsp;Account</div>
             </a>
-            <a href="/" className="logout-link">
+            <a href="/" onClick={this.logout} className="logout-link">
               <div className="settings-dropdown__item">Logout</div>
             </a>
           </div>
