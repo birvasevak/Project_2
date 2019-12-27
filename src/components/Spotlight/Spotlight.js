7a8a3bf8503696efb9cc68e4810d03a56b037b70
EuphyD <47607574+EuphyD@users.noreply.github.com>
2019-12-26 16:27:43 -0900

    Spt_downarrowFix
    Fix the down arrow part

--- src/components/Spotlight/Spotlight.js
+++ src/components/Spotlight/Spotlight.js
@@ -6,8 +6,7 @@ class Spotlight extends React.Component {
   render() {
     return (
       <div>
-        <SptRightSide />
-        {/* <SidebarProfile /> */}
+          <SptRightSide />
       </div>
     );
   }
